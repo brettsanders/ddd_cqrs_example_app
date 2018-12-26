534f7df847ad08cd04156532b23a65306dee82cc
Brett Sanders <brettrsanders@gmail.com>
2018-12-26 15:18:27 -0500

    Wallet Index & Show View tweaks

--- app/controllers/wallets_controller.rb
+++ app/controllers/wallets_controller.rb
@@ -17,7 +17,7 @@ class WalletsController < ApplicationController
 
     new_wallet = WalletsReadModel::Wallet.find_by_uid(cmd.wallet_id)
     if new_wallet
-      redirect_to wallet_path(new_wallet), notice: 'New Wallet Created'
+      redirect_to wallet_path(new_wallet.uid), notice: 'New Wallet Created'
     else
       flash[:error] = "Unable to create new wallet"
       redirect_to new_wallet_path
